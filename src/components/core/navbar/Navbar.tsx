a1e306d351e6e6edd8f7c1341db2824e2cddab51
hadyelzayady <hadyelzayady1996@gmail.com>
2024-09-26 09:14:32 +0300

    logo

--- src/components/core/navbar/Navbar.tsx
+++ src/components/core/navbar/Navbar.tsx
@@ -1,12 +1,14 @@
 import { A } from "@solidjs/router";
 import { navbarItems } from "./navbarItems";
 import { For } from "solid-js";
+import image from "../../../assets/hz-softwares.webp"
 
 export function Navbar() {
 	return (
 		<div class="navbar bg-base-300 rounded-box">
 			<div class="flex-1 px-2 lg:flex-none">
-				<a class="text-lg font-bold">Blazingly Fast Services</a>
+      <img width="50" height="50" src="/images/hz-softwares.png"/>
+				<a class="text-lg font-bold">HZ Software</a>
 			</div>
 			<div class="flex justify-end flex-1 px-2">
 				<div class="flex items-stretch">
