8d1303c2e044e913143dbba2ecea40dae6a01a27
hadyelzayady <hadyelzayady1996@gmail.com>
2024-12-06 21:32:40 +0200

    add arch package link

--- src/pages/top-sort/TopSortPage.tsx
+++ src/pages/top-sort/TopSortPage.tsx
@@ -4,7 +4,7 @@ import { Header } from "../../components/shared/headers/Header";
 export function TopSortPage() {
 	return (
 		<div>
-			<Header.h2>Arch Package Dependencies Topological Sort</Header.h2>
+			<Header.h2>Arch Package Dependencies Topological Sort <a class="link link-primary" target="_blank" href="https://archlinux.org/packages">arch packages</a></Header.h2>
 			<DependencyTopSort />
 		</div>
 	);
