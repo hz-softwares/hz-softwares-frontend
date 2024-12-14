a3263b28aaee656ceb94f52020e79c424146bab7
hadyelzayady <hadyelzayady1996@gmail.com>
2024-12-14 18:23:45 +0200

    add maring after logo

--- src/components/core/navbar/Navbar.tsx
+++ src/components/core/navbar/Navbar.tsx
@@ -6,7 +6,7 @@ export function Navbar() {
 	return (
 		<div class="navbar bg-base-300 rounded-box">
 			<div class="flex-1 px-2 lg:flex-none">
-				<img width="50" height="50" src="/images/hz-softwares.png" />
+				<img class="mr-1" width="50" height="50" src="/images/hz-softwares.png" />
 				<a class="text-lg font-bold">HZ Software</a>
 			</div>
 			<div class="flex justify-end flex-1 px-2">
