1ddf64b7a5f586a0d8d99d42babe768145ed52ef
hadyelzayady <hadyelzayady1996@gmail.com>
2024-12-14 16:23:53 +0200

    fix home page

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,8 +1,7 @@
 export function HomePage() {
 	return (
 		<div>
-			<h1 class="text-3xl font-bold underline">Hello world!</h1>
-			<button class="btn">Hello daisyUI</button>
+			<h1 class="text-3xl font-bold underline">Welcome to hz-software</h1>
 		</div>
 	);
 }
