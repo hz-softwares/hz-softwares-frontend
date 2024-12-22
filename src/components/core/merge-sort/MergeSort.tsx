7b236b0ee5baec4494b4070e0c5f25d03f693209
hadyelzayady <hadyelzayady1996@gmail.com>
2024-12-22 10:43:57 +0200

    merge sort

--- src/components/core/merge-sort/MergeSort.tsx
+++ src/components/core/merge-sort/MergeSort.tsx
@@ -1,12 +1,13 @@
-import { createSignal, Show } from "solid-js";
+import { createDeferred, createSignal, Show } from "solid-js";
 import { Input } from "../../shared/input/Input";
 import { createWorker } from "@solid-primitives/workers";
-import { Button } from "../../shared/button/Button";
 import styles from "./mergeSort.module.css";
 import { useMergeSort } from "./hooks/useMergeSort";
 interface Props {}
-export function MergeSort(props: Props) {
+export function MergeSort() {
 	const [getGenerated, setGenerated] = createSignal<number[]>([]);
+	const [getListString, setListString] = createSignal("");
+	const [getGenerateLoading, setGenerateLoading] = createSignal(false);
 	const [getSorted, setSorted] = createSignal<number[]>([]);
 	const [worker, start, stop] = createWorker(function generateRandomListOfLength(length: number) {
 		function getRandomInt(max: number) {
@@ -18,30 +19,37 @@ export function MergeSort(props: Props) {
 		}
 		return list;
 	});
-	const { loading, mergeSort } = useMergeSort();
+	const { mergeSort } = useMergeSort();
+	const defer = createDeferred(() => `[${mergeSort(getGenerated()).slice(0, 100).join(", ")}]`);
 
 	return (
 		<div>
+			<details>
+				<summary>Merge Sort</summary>
+				<p>
+					Used for sorting large dataset or data on disk as merge sort works well with sequential access which comply
+					with disk access
+				</p>
+			</details>
 			<div class={styles.inputSortContainer}>
 				<Input
 					type="number"
-					max={1000000}
+					max={"10000"}
 					onKeyPress={async (e) => {
 						if (e.key === "Enter") {
 							const length = e.currentTarget.valueAsNumber;
+							setGenerateLoading(true);
 							const list = await worker.generateRandomListOfLength(length);
+							setGenerateLoading(false);
+							setListString(`[${list.slice(0, 100).join(",")}]`);
 							setGenerated(list);
-							setSorted(await mergeSort(getGenerated()));
 						}
 					}}
 				/>
-				<Button onClick={() => setSorted(mergeSort(getGenerated()))}> Sort</Button>
 			</div>
-			<Show when={getGenerated().length > 0}>
-				<div class={`${styles.generatedLine} truncate`}>Generated: [{getGenerated().join(", ")}] </div>
-			</Show>
-			<Show when={getSorted().length > 0 && !loading()}>
-				<div class={`${styles.generatedLine} truncate`}>sorted: [{getSorted().join(", ")}] </div>
+			<Show when={!getGenerateLoading()} fallback={"...generating"}>
+				<div class={`${styles.generatedLine} truncate`}>Generated: {getListString()} </div>
+				<div class={`${styles.generatedLine} truncate`}>Sorted: {defer()} </div>
 			</Show>
 		</div>
 	);
--- src/components/core/merge-sort/hooks/useMergeSort.tsx
+++ src/components/core/merge-sort/hooks/useMergeSort.tsx
@@ -3,12 +3,49 @@ import { createSignal } from "solid-js";
 
 export function useMergeSort() {
 	const [loading, setLoading] = createSignal(false);
-	const [pool, start, stop] = createWorkerPool(4, function mergeSort(list: number[]) {});
+	const [pool, start, stop] = createWorkerPool(4, function sort(list: number[]) {
+		function divide(list: number[]) {
+			const half = list.length / 2;
+			return [list.slice(0, half), list.slice(half)];
+		}
+
+		function join(left: number[], right: number[]) {
+			let leftIndex = 0;
+			let rightIndex = 0;
+			const resultList = [];
+			while (leftIndex < left.length && rightIndex < right.length) {
+				if (left[leftIndex] <= right[rightIndex]) {
+					resultList.push(left[leftIndex]);
+					leftIndex = leftIndex + 1;
+				} else if (rightIndex < right.length) {
+					resultList.push(right[rightIndex]);
+					rightIndex = rightIndex + 1;
+				}
+			}
+			if (leftIndex < left.length) {
+				resultList.push(...left.slice(leftIndex));
+			} else if (rightIndex < right.length) {
+				resultList.push(...right.slice(rightIndex));
+			}
+			return resultList;
+		}
+		function mergeSort(list: number[]): number[] {
+			if (list.length < 2) {
+				return list;
+			}
+			const [left, right] = divide(list);
+			const leftSorted = mergeSort(left);
+			const rightSorted = mergeSort(right);
+			return join(leftSorted, rightSorted);
+		}
+
+	});
 
 	function divide(list: number[]) {
 		const half = list.length / 2;
 		return [list.slice(0, half), list.slice(half)];
 	}
+
 	function join(left: number[], right: number[]) {
 		let leftIndex = 0;
 		let rightIndex = 0;
