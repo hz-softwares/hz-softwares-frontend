cda41a95fcb2360d1ddc80ed4cd74d78685f43fe
hadyelzayady <hadyelzayady1996@gmail.com>
2024-12-27 19:37:37 +0200

    merge sort

--- src/components/core/merge-sort/MergeSort.tsx
+++ src/components/core/merge-sort/MergeSort.tsx
@@ -3,29 +3,40 @@ import { Input } from "../../shared/input/Input";
 import { createWorker } from "@solid-primitives/workers";
 import styles from "./mergeSort.module.css";
 import { useMergeSort } from "./hooks/useMergeSort";
-interface Props {}
+import { VirtualList } from "./components/VirtualList";
+
+const MAX_LENGTH = 50_000_0000;
 export function MergeSort() {
+	const [value, setValue] = createSignal<number>();
 	const [getGenerated, setGenerated] = createSignal<number[]>([]);
-	const [getListString, setListString] = createSignal("");
-	const [getGenerateLoading, setGenerateLoading] = createSignal(false);
-	const [getSorted, setSorted] = createSignal<number[]>([]);
+	const [loading, setLoading] = createSignal(false);
+	const [generating, setGenerating] = createSignal(false);
+
 	const [worker, start, stop] = createWorker(function generateRandomListOfLength(length: number) {
 		function getRandomInt(max: number) {
 			return Math.floor(Math.random() * max);
 		}
-		const list = [];
+		const list = new Set();
 		for (let index = 0; index < length; index++) {
-			list.push(getRandomInt(length));
+			list.add(getRandomInt(length));
 		}
-		return list;
+		return Array.from(list);
 	});
+
 	const { mergeSort } = useMergeSort();
-	const defer = createDeferred(() => `[${mergeSort(getGenerated()).slice(0, 100).join(", ")}]`);
+	const sortedList = createDeferred(() => {
+		console.log("merge");
+		setLoading(true);
+		const result = mergeSort(getGenerated());
+		console.log("finish merge");
+		setLoading(false);
+		return result;
+	});
 
 	return (
 		<div>
 			<details>
-				<summary>Merge Sort</summary>
+				<summary>Merge Sort (upto {MAX_LENGTH} using web workers to not block ui when generating large list)</summary>
 				<p>
 					Used for sorting large dataset or data on disk as merge sort works well with sequential access which comply
 					with disk access
@@ -35,22 +46,45 @@ export function MergeSort() {
 				<Input
 					type="number"
 					max={"10000"}
+					value={value()}
+					onInput={(e) => {
+						const number = e.currentTarget.valueAsNumber;
+						if (number > MAX_LENGTH) {
+							setValue(MAX_LENGTH);
+						} else {
+							setValue(number);
+						}
+					}}
 					onKeyPress={async (e) => {
 						if (e.key === "Enter") {
 							const length = e.currentTarget.valueAsNumber;
-							setGenerateLoading(true);
-							const list = await worker.generateRandomListOfLength(length);
-							setGenerateLoading(false);
-							setListString(`[${list.slice(0, 100).join(",")}]`);
-							setGenerated(list);
+							try {
+								setGenerating(true);
+								const list = await (worker as any).generateRandomListOfLength(length);
+								console.log("list");
+								setGenerated(list);
+							} finally {
+								setGenerating(false);
+								console.log("hhh");
+							}
 						}
 					}}
 				/>
 			</div>
-			<Show when={!getGenerateLoading()} fallback={"...generating"}>
-				<div class={`${styles.generatedLine} truncate`}>Generated: {getListString()} </div>
-				<div class={`${styles.generatedLine} truncate`}>Sorted: {defer()} </div>
-			</Show>
+			<div class={styles.listsContainer}>
+				<div class={styles.list}>
+					<span>Generated List (#elements:{getGenerated().length})</span>
+					<Show when={!generating()} fallback={"...loading"}>
+						<VirtualList items={getGenerated()} />
+					</Show>
+				</div>
+				<div class={styles.list}>
+					<span>Sorted List</span>
+					<Show when={!loading() && !generating()} fallback={"...loading"}>
+						<VirtualList items={sortedList().toReversed()} />
+					</Show>
+				</div>
+			</div>
 		</div>
 	);
 }
---
+++ src/components/core/merge-sort/components/VirtualList.tsx
@@ -0,0 +1,59 @@
+import { createVirtualizer } from "@tanstack/solid-virtual";
+import { createEffect, For } from "solid-js";
+
+interface Props {
+	items: any[];
+}
+export function VirtualList(props: Props) {
+	let parentRef: HTMLDivElement | undefined = undefined;
+
+	const rowVirtualizer = createVirtualizer({
+		count: props.items.length,
+		getScrollElement: () => parentRef ?? null,
+		estimateSize: () => 20,
+		enabled: false,
+	});
+	createEffect(() => {
+		rowVirtualizer.setOptions({ ...rowVirtualizer.options, enabled: false });
+		rowVirtualizer._willUpdate();
+		rowVirtualizer.setOptions({ ...rowVirtualizer.options, count: props.items.length, enabled: true });
+		rowVirtualizer._willUpdate();
+	});
+
+	return (
+		<div
+			ref={parentRef}
+			style={{
+				height: `400px`,
+				overflow: "auto", // Make it scroll!
+				border: "1px solid white",
+				width: "100px",
+			}}
+		>
+			<div
+				style={{
+					height: `${rowVirtualizer.getTotalSize()}px`,
+					width: "100%",
+					position: "relative",
+				}}
+			>
+				<For each={rowVirtualizer.getVirtualItems()}>
+					{(virtualItem) => (
+						<div
+							style={{
+								position: "absolute",
+								top: 0,
+								left: 0,
+								width: "100%",
+								height: `${virtualItem.size}px`,
+								transform: `translateY(${virtualItem.start}px)`,
+							}}
+						>
+							{props.items[virtualItem.index]}
+						</div>
+					)}
+				</For>
+			</div>
+		</div>
+	);
+}
