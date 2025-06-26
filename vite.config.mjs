71ff939835bdd9a18f4132bf5b6437797d05e16f
hadyelzayady <hadyelzayady1996@gmail.com>
2025-06-27 01:39:44 +0300

    init

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,15 +4,12 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
-	plugins: [
-		/*
-    Uncomment the following line to enable solid-devtools.
-    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
-    */
-		devtools(),
-		commonjs({ exclude: ["node_modules/lodash/*"] }), // Add CommonJS plugin
-		solidPlugin(),
-	],
+	plugins: [devtools(), solidPlugin()],
+	resolve: {
+		alias: {
+			"~": path.resolve(__dirname, "./src"),
+		},
+	},
 	server: {
 		port: 3000,
 		strictPort: true,
